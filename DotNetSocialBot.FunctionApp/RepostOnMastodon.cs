548e7015af8592d52ea5bb5c938f257d6aacc30b
bitbonk <bitbonk@msn.com>
2022-12-10 22:50:40 +0100

    Add hashtag boosting functionality

--- DotNetSocialBot.FunctionApp/RepostOnMastodon.cs
+++ DotNetSocialBot.FunctionApp/RepostOnMastodon.cs
@@ -15,24 +15,48 @@ public class RepostOnMastodon
 #if DEBUG // TODO: find out why certs are not accepted locally
         handler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
 #endif
-        this.client = new MastodonClient(
-            new AppRegistration { Instance = Config.Instance },
-            new Auth { AccessToken = Config.AccessToken },
-            new HttpClient(handler));
+        client = new MastodonClient(Config.Instance, Config.AccessToken, new HttpClient(handler));
     }
 
 
     [FunctionName("RepostOnMastodon")]
     public async Task Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger log)
     {
+        await BoostMentions(log);
+        await BoostTags(log);
+    }
+
+    private async Task BoostTags(ILogger log)
+    {
+        // TODO: Get the tags from the followed tags, instead of hardcoding "dotnet"
+        // TODO: Get "my" AccountName instead of hard coding it
+        foreach (var status in (await client.GetHomeTimeline(new ArrayOptions { Limit = 100 })).Where(s =>
+                     !s.IsReply()
+                     && s.Reblogged != true
+                     && s.Reblog is null
+                     && s.Account.Bot != true
+                     && !s.Account.AccountName.Equals("bot@dotnet.social", StringComparison.InvariantCultureIgnoreCase)
+                     && s.Tags.Any(t =>
+                         t.Name.Equals("dotnet", StringComparison.InvariantCultureIgnoreCase))))
+        {
+            await client.Reblog(status.Id);
+            log.LogInformation("Boosted post with hashtag by @{Account} from {PostTime}", status.Account.AccountName,
+                status.CreatedAt);
+
+        }
+    }
+
+    private async Task BoostMentions(ILogger log)
+    {
         var notifications = await client
-            .GetNotifications(excludeTypes: NotificationType.Follow | NotificationType.Favourite | NotificationType.Reblog);
+            .GetNotifications(excludeTypes: NotificationType.Follow | NotificationType.Favourite |
+                                            NotificationType.Reblog);
         foreach (var notification in notifications
-            .Where(n => !n.Status.IsReply() && n.Status.Account.Bot != true))
+                     .Where(n => !n.Status.IsReply() && n.Status.Account.Bot != true))
         {
             await client.Reblog(notification.Status.Id);
-            log.LogInformation("Boosted post by @{Account} from {PostTime}", notification.Account.AccountName, notification.Status.CreatedAt);
-
+            log.LogInformation("Boosted post that mentioned me by @{Account} from {PostTime}", notification.Account.AccountName,
+                notification.Status.CreatedAt);
         }
 
         await client.ClearNotifications();
